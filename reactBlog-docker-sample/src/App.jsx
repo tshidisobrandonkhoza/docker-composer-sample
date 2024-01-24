60b591a8b8498342113c5e8fdac1bb4b9eb9621f
brandonkhoza <8brandonkhoza@gmail.com>
2024-01-24 22:45:08 +0200

    Combine the API and the FRONTEND

--- reactBlog-docker-sample/src/App.jsx
+++ reactBlog-docker-sample/src/App.jsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react"
 
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:4000/').then(res => res.json()).then(data => setBlogs(data))
+    // effect
+    // return () => {
+    //   cleanup
+    // };
+  }, []);
 
   return (
     <>
       <div className='main-bg'>
-        Hello, To The World
+        <h1>I guess i miss designing - The developer </h1>
+        {!blogs ? <p>Loading</p> :
+          blogs.slice(0, 6).map(myblog => (
+
+            <div className="blog-body" key={myblog.div}>
+              <h3>{myblog.title}</h3>
+              <p>{myblog.body}</p>
+            </div>
+
+          ))
+        }
       </div>
     </>
   )
--- reactBlog-docker-sample/vite.config.js
+++ reactBlog-docker-sample/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+
+  },
 })
